121932c40044e8dd5fd676e826bc2f70c9a36fb5
Segun <segunolalive@gmail.com>
2020-03-15 09:38:07 +0100

    add base cms config

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,61 @@
+const fs = require('fs');
+// Import filters
+const dateFilter = require('./src/filters/date-filter.js');
+const markdownFilter = require('./src/filters/markdown-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
+const site = require('./src/_data/site.json');
+
 module.exports = function(config) {
   config.addPassthroughCopy('themekit');
   config.addPassthroughCopy('media');
   config.addPassthroughCopy('static');
+  config.addPassthroughCopy('src/admin/config.yml');
+  config.addPassthroughCopy('src/admin/utils.js');
+
+  // Filters
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('markdownFilter', markdownFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
+
+  const now = new Date();
+
+  // Custom collections
+  const livePosts = post => post.date <= now && !post.data.draft;
+  config.addCollection('posts', collection => {
+    return [
+      ...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)
+    ].reverse();
+  });
+
+  config.addCollection('postFeed', collection => {
+    return [
+      ...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)
+    ]
+      .reverse()
+      .slice(0, site.maxPostsPerPage);
+  });
+
+  // 404
+  config.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, browserSync) {
+        const content_404 = fs.readFileSync('dist/404.html');
+
+        browserSync.addMiddleware('*', (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
 
   return {
     dir: {
       input: 'src',
       output: 'dist'
     },
-    passthroughFileCopy: true,
-    markdownTemplateEngine: 'njk',
-    templateFormats: ['html', 'njk', 'md']
+    passthroughFileCopy: true
   };
 };
