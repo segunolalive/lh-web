205fb1365d01b682f530851beecf04816ace5e83
Segun <segunolalive@gmail.com>
2020-03-25 21:53:42 +0100

    update title to headline

--- src/admin/preview-templates/banner.js
+++ src/admin/preview-templates/banner.js
@@ -4,23 +4,20 @@ const html = htm.bind(h);
 
 const Banner = createClass({
   render() {
-    // const entry = this.props.entry;
-    // const title = entry.getIn(['data', 'title']);
-    // const subTitle = entry.getIn(['data', 'subTitle']);
 
-    const { title, subTitle } = this.props.entry.getIn(['data']).toJS()
+    const { title, subHead } = this.props.entry.getIn(['data']).toJS();
 
     return html`
-    <header class="header-image ken-burn-center light">
-      <div class="container">
-        <hr class="space-lg"/>
-        <h1>${title}</h1>
-        <h2>${subTitle}</h2>
-        <hr class="space" />
-        <hr class="space-lg" />
-      </div>
-    </header>
-      `;
+      <header class="header-image ken-burn-center light">
+        <div class="container">
+          <hr class="space-lg" />
+          <h1>${title}</h1>
+          <h2>${subHead}</h2>
+          <hr class="space" />
+          <hr class="space-lg" />
+        </div>
+      </header>
+    `;
   }
 });
 
--- src/admin/preview-templates/home.js
+++ src/admin/preview-templates/home.js
@@ -4,26 +4,25 @@ const html = htm.bind(h);
 
 const Home = createClass({
   render() {
-    const entry = this.props.entry;
-    const title = entry.getIn(['data', 'title']);
-    const subTitle = entry.getIn(['data', 'subTitle']);
-    const banner = entry.getIn(['data', 'banner']);
+    const { title, subHead, banner } = this.props.entry.getIn(['data']).toJS();
     const action = { text: 'Get Started', url: '/signup' };
 
-  console.log(this.props.widgetFor('banner'))
+    console.log(this.props.widgetFor('banner'));
     return html`
-    <header class="header-image ken-burn-center dark preview-banner">
-      <img class="" src="/media/${banner}" alt="banner" />
-      <div class="container preview-banner__image">
-        <hr class="space-lg"/>
-        <h1>${title}</h1>
-        <h2>${subTitle}</h2>
-        <hr class="space" />
-        <a href="${action.url}" class="btn btn-circle btn-sm">${action.text}</a>
-        <hr class="space-lg" />
-      </div>
-    </header>
-      `;
+      <header class="header-image ken-burn-center dark preview-banner">
+        <img class="" src="/media/${banner}" alt="banner" />
+        <div class="container preview-banner__image">
+          <hr class="space-lg" />
+          <h1>${title}</h1>
+          <h2>${subHead}</h2>
+          <hr class="space" />
+          <a href="${action.url}" class="btn btn-circle btn-sm"
+            >${action.text}</a
+          >
+          <hr class="space-lg" />
+        </div>
+      </header>
+    `;
   }
 });
 
